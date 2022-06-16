3181cf6f158128a5bc8fac904a0c6cae14fd8a7c
dam0cl3s <damm084@tutanota.com>
2022-06-16 13:17:00 -0500

    Typescript and vite configurations

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
 	plugins: [react()],
 	envDir: 'env',
 
@@ -22,4 +22,4 @@ export default defineConfig(({ mode }) => ({
 		// ssrManifest: true,
 		minify: true,
 	},
-}));
+});
